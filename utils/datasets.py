3a1168525310c8fbac3c80972ce444bda9fa5a99
Marco Oliva <marco_oliva@icloud.com>
2022-12-08 18:41:45 +0100

    Update datasets.py

--- utils/datasets.py
+++ utils/datasets.py
@@ -387,7 +387,7 @@ class LoadImagesAndLabels(Dataset):  # for training/testing
 
         # Check cache
         self.label_files = img2label_paths(self.img_files)  # labels
-        cache_path = (p if p.is_file() else Path().absolute().with_suffix('.cache')  # cached labels
+        cache_path = p if p.is_file() else Path().absolute().with_suffix('.cache')  # cached labels
         if cache_path.is_file():
             cache, exists = torch.load(cache_path), True  # load
             #if cache['hash'] != get_hash(self.label_files + self.img_files) or 'version' not in cache:  # changed
